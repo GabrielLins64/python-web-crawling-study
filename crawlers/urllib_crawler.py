3f22712441a5b775cbc857ab89a9ca05e0cb6ccb
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-05-13 11:18:08 -0300

    refactor: autopep8

--- crawlers/urllib_crawler.py
+++ crawlers/urllib_crawler.py
@@ -16,13 +16,14 @@ class UrlLibCrawler(Crawler):
         result = dict()
         for source in self.sources:
             req = Request(source['url'],
-                        headers=self.headers)
+                          headers=self.headers)
             html = urlopen(req).read()
             html = str(html)
 
             for data in source['data']:
                 regex = re.search(data['find_regex'], html)
-                regex_result = regex.group(1) if hasattr(regex, "groups") else "Not found"
+                regex_result = \
+                    regex.group(1) if hasattr(regex, "groups") else "Not found"
                 result[data['result_name']] = regex_result
 
         return result
